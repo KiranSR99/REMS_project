c9f02b6665f0d6cdc347355310d536267291f655
Kiran Shrestha <kiranxtha99@gmail.com>
2023-05-31 18:30:22 +0545

    Modifications made to the files structures. and maintained the style.css file

---
+++ includes/footer.php
@@ -0,0 +1,55 @@
+<!-- <?php
+include 'header.php';
+?> -->
+
+<head>
+    <link rel="stylesheet" href="../CSS/style.css">
+</head>
+<section class="footer">
+    <div class="container">
+        <div id="footer">
+            <div class="footer-column">
+                <h2 class="title-footer">Hill Paradise Resort</h2>
+                <p>Kathmandu, Nepal</p>
+                <p>Phone: 9812345678</p>
+                <p>hillparadise@sample.com</p>
+            </div>
+
+            <div class="footer-column">
+                <h2 class="title-footer">Quick Links</h2>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Special Offers</a></li>
+                    <li><a href="#">Sitemap</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-column">
+                <h2 class="title-footer">Newsletter</h2>
+                <input type="email" name="email" class="email" placeholder="Enter Your Email"><br>
+                <input class="btn-small" type="submit" value="Submit">
+
+                <ul class="social-links">
+                    <li><a href="#"><img src="../assets/images/fb_icon.png" alt=""></a></li>
+                    <li><a href="#"><img src="../assets/images/insta_icon.png" alt=""></a></li>
+                    <li><a href="#"><img src="../assets/images/tiktok_icon.png" alt=""></a></li>
+                    <li><a href="#"><img src="../assets/images/tripAdvisor.png" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="copyright">
+            <hr>
+            <p class="text">
+                Copyright © 2023, Hill Paradise Resort. All Right Reserve. <a href="#">Privacy policy</a>
+            </p>
+        </div>
+    </div>
+</section>
+
+</main>
+
+
+<script src="../JS/script.js"></script>
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hill Paradise Resort</title>
+
+    <link rel="shortcut icon" href="./assets/images/HillParadise.png" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/style.css">
+</head>
+
+<body>
+    <main>
+        <header>
+            <div class="header">
+                <div class="logo">
+                    <a href="#"><img src="../assets/images/HillParadise.png" alt="LOGO"></a>
+                </div>
+
+                <div class="menus">
+                    <div class="menu-container">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Events</a></li>
+                            <li><a href="#">Packages</a></li>
+                            <li><a href="#">Gallery</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a class="btn" href="#">Reserve Now</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="contact-container">
+                        <ul>
+                            <li><a class="number" href="#">9812345678</a></li>
+                            <li><a href="#"><img src="../assets/images/fb_icon.png" alt="fb"></a></li>
+                            <li><a href="#"><img src="../assets/images/insta_icon.png" alt="insta"></a></li>
+                            <li><a href="#"><img src="../assets/images/tiktok_icon.png" alt="tiktok"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
